795bab6306ab381b5ef4345859015392bbf4611f
Lucas Morales <lucas@lucasem.com>
2018-02-17 01:17:43 -0500

    parse

--- src/expression.rs
+++ src/expression.rs
@@ -9,8 +9,8 @@ use std::fmt;
 
 #[derive(Debug, Clone)]
 pub enum InferenceError {
-    BadPrimitive(String),
-    BadInvented(u64),
+    BadPrimitive(usize),
+    BadInvented(usize),
     Unify(polytype::UnificationError),
 }
 impl From<polytype::UnificationError> for InferenceError {
@@ -21,7 +21,7 @@ impl From<polytype::UnificationError> for InferenceError {
 impl fmt::Display for InferenceError {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         match self {
-            &InferenceError::BadPrimitive(ref s) => write!(f, "invalid primitive: '{}'", s),
+            &InferenceError::BadPrimitive(i) => write!(f, "invalid primitive: '{}'", i),
             &InferenceError::BadInvented(i) => write!(f, "invalid invented: '{}'", i),
             &InferenceError::Unify(ref err) => write!(f, "could not unify to infer type: {}", err),
         }
@@ -34,7 +34,7 @@ impl ::std::error::Error for InferenceError {
 }
 
 #[derive(Debug, Clone)]
-pub struct ParseError(u64, &'static str);
+pub struct ParseError(usize, &'static str);
 impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         write!(f, "{} at index {}", self.1, self.0)
@@ -52,52 +52,70 @@ impl ::std::error::Error for ParseError {
 
 /// An expression context is effectively a registry for primitive and invented expressions.
 ///
-/// Most expressions don't make sense without a context.
-#[derive(Debug, Clone)]
-pub struct Context<'a, 'c: 'a> {
-    primitives: HashMap<&'c str, Type>,
-    invented: Vec<(Variant<'a>, Type)>,
+/// Most expressions don't make sense without a context, hence the requirement of one for any
+/// expression.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Context {
+    primitives: Vec<(String, Type)>,
+    invented: Vec<(Variant, Type)>,
 }
-impl<'a, 'c> Context<'a, 'c> {
-    pub fn new(primitives: HashMap<&'c str, Type>, invented: Vec<(Variant<'a>, Type)>) -> Self {
+impl Context {
+    pub fn new(primitives: Vec<(String, Type)>, invented: Vec<(Variant, Type)>) -> Self {
         Context {
             primitives,
             invented,
         }
     }
-    pub fn invent(&mut self, expr: Variant<'a>) -> u64 {
+    pub fn invent(&mut self, expr: Variant) -> usize {
         let mut tctx = polytype::Context::default();
         let env = VecDeque::new();
         let mut indices = HashMap::new();
         let tp = expr.infer_internal(&self, &mut tctx, &env, &mut indices)
             .expect("invalid invention");
         self.invented.push((expr, tp));
-        (self.invented.len() - 1) as u64
+        self.invented.len() - 1
+    }
+    pub fn parse(&self, inp: &str) -> Result<Expression, ParseError> {
+        let s = inp.trim_left();
+        let offset = inp.len() - s.len();
+        Variant::parse(self, s, offset).and_then(move |(di, variant)| {
+            if s[di..].chars().all(char::is_whitespace) {
+                Ok(Expression {
+                    ctx: &self,
+                    variant,
+                })
+            } else {
+                Err(ParseError(
+                    offset + di,
+                    "expected end of expression, found more tokens",
+                ))
+            }
+        })
     }
 }
 
-#[derive(Debug, Clone)]
-pub enum Variant<'a> {
-    Primitive(&'a str),
-    Application(Box<Variant<'a>>, Box<Variant<'a>>),
-    Abstraction(Box<Variant<'a>>),
+#[derive(Debug, Clone, PartialEq)]
+pub enum Variant {
+    Primitive(usize),
+    Application(Box<Variant>, Box<Variant>),
+    Abstraction(Box<Variant>),
     Index(u64),
 
-    Invented(u64),
+    Invented(usize),
 }
-impl<'a> Variant<'a> {
-    fn infer_internal<'c: 'a>(
+impl Variant {
+    fn infer_internal(
         &self,
-        ctx: &Context<'a, 'c>,
+        ctx: &Context,
         mut tctx: &mut polytype::Context,
         env: &VecDeque<Type>,
         indices: &mut HashMap<u64, Type>,
     ) -> Result<Type, InferenceError> {
         match self {
-            &Variant::Primitive(name) => if let Some(tp) = ctx.primitives.get(name) {
-                Ok(tp.instantiate_indep(tctx))
+            &Variant::Primitive(num) => if let Some(prim) = ctx.primitives.get(num as usize) {
+                Ok(prim.1.instantiate_indep(tctx))
             } else {
-                Err(InferenceError::BadPrimitive(String::from(name)))
+                Err(InferenceError::BadPrimitive(num))
             },
             &Variant::Application(ref f, ref x) => {
                 let f_tp = f.infer_internal(ctx, &mut tctx, env, indices)?;
@@ -123,18 +141,16 @@ impl<'a> Variant<'a> {
                         .apply(tctx))
                 }
             }
-            &Variant::Invented(num) => {
-                if let Some(inv) = ctx.invented.get(num as usize) {
-                    Ok(inv.1.instantiate_indep(tctx))
-                } else {
-                    Err(InferenceError::BadInvented(num))
-                }
-            }
+            &Variant::Invented(num) => if let Some(inv) = ctx.invented.get(num as usize) {
+                Ok(inv.1.instantiate_indep(tctx))
+            } else {
+                Err(InferenceError::BadInvented(num))
+            },
         }
     }
-    fn show<'c: 'a>(&self, ctx: &Context<'a, 'c>, is_function: bool) -> String {
+    fn show(&self, ctx: &Context, is_function: bool) -> String {
         match self {
-            &Variant::Primitive(name) => String::from(name),
+            &Variant::Primitive(num) => ctx.primitives[num as usize].0.clone(),
             &Variant::Application(ref f, ref x) => if is_function {
                 format!("{} {}", f.show(ctx, true), x.show(ctx, false))
             } else {
@@ -147,14 +163,148 @@ impl<'a> Variant<'a> {
             }
         }
     }
+    /// inp must not have leading whitespace. Does not invent.
+    fn parse(
+        ctx: &Context,
+        inp: &str,
+        offset: usize, // for good error messages
+    ) -> Result<(usize, Variant), ParseError> {
+        let init: Option<Result<(usize, Variant), ParseError>> = None;
+
+        let primitive = || {
+            match inp.find(|c: char| c.is_whitespace() || c == ')') {
+                None if inp.len() > 0 => Some(inp.len()),
+                Some(next) if next > 0 => Some(next),
+                _ => None,
+            }.map(|di| {
+                if let Some(num) = ctx.primitives
+                    .iter()
+                    .position(|&(ref name, _)| name == &inp[..di])
+                {
+                    Ok((di, Variant::Primitive(num)))
+                } else {
+                    Err(ParseError(offset + di, "unexpected end of expression"))
+                }
+            })
+        };
+        let application = || {
+            inp.find('(')
+                .and_then(|i| {
+                    if inp[..i].chars().all(char::is_whitespace) {
+                        Some(i + 1)
+                    } else {
+                        None
+                    }
+                })
+                .map(|mut di| {
+                    let mut items = VecDeque::new();
+                    loop {
+                        // parse expr
+                        let (ndi, variant) = Variant::parse(ctx, &inp[di..], offset + di)?;
+                        items.push_back(variant);
+                        di += ndi;
+                        // skip spaces
+                        di += inp[di..].chars().take_while(|c| c.is_whitespace()).count();
+                        // check if complete
+                        match inp.chars().nth(di) {
+                            None => break Err(ParseError(offset + di, "incomplete application")),
+                            Some(')') => {
+                                di += 1;
+                                break if let Some(init) = items.pop_front() {
+                                    let app = items.into_iter().fold(init, |a, v| {
+                                        Variant::Application(Box::new(a), Box::new(v))
+                                    });
+                                    Ok((di, app))
+                                } else {
+                                    Err(ParseError(offset + di, "empty application"))
+                                };
+                            }
+                            _ => (),
+                        }
+                    }
+                })
+        };
+        let abstraction = || {
+            inp.find('(')
+                .and_then(|i| {
+                    if inp[..i].chars().all(char::is_whitespace) {
+                        Some(i + 1)
+                    } else {
+                        None
+                    }
+                })
+                .and_then(|di| match inp[di..].find(char::is_whitespace) {
+                    Some(ndi) if &inp[di..di + ndi] == "lambda" || &inp[di..di + ndi] == "λ" => {
+                        Some(di + ndi)
+                    }
+                    _ => None,
+                })
+                .map(|mut di| {
+                    // skip spaces
+                    di += inp[di..].chars().take_while(|c| c.is_whitespace()).count();
+                    // parse body
+                    let (ndi, body) = Variant::parse(ctx, &inp[di..], offset + di)?;
+                    di += ndi;
+                    // check if complete
+                    inp.chars()
+                        .nth(di)
+                        .and_then(|c| if c == ')' { Some(di + 1) } else { None })
+                        .ok_or(ParseError(offset + di, "incomplete application"))
+                        .map(|di| (di, Variant::Abstraction(Box::new(body))))
+                })
+        };
+        let index = || {
+            if inp.chars().nth(0) == Some('$') && inp.len() > 1 {
+                inp[1..]
+                    .find(|c: char| c.is_whitespace() || c == ')')
+                    .and_then(|i| inp[1..1 + i].parse::<u64>().ok().map(|num| (1 + i, num)))
+                    .map(|(di, num)| Ok((di, Variant::Index(num))))
+            } else {
+                None
+            }
+        };
+        let invented = || {
+            if inp.chars().take(2).collect::<String>() == "#(" {
+                Some(1)
+            } else {
+                None
+            }.map(|mut di| {
+                let (ndi, variant) = Variant::parse(ctx, &inp[di..], offset + di)?;
+                di += ndi;
+                if let Some(num) = ctx.invented
+                    .iter()
+                    .position(|&(ref var, _)| var == &variant)
+                {
+                    Ok((di, Variant::Invented(num)))
+                } else {
+                    Err(ParseError(
+                        offset + di,
+                        "invented expr is unfamiliar to context",
+                    ))
+                }
+            })
+        };
+        // These parsers return None if the variant isn't applicable
+        // or Some(Err(..)) if the variant applied but was invalid.
+        // It is imperative that primitive comes last.
+        init.or_else(abstraction)
+            .or_else(application)
+            .or_else(index)
+            .or_else(invented)
+            .or_else(primitive)
+            .unwrap_or(Err(ParseError(
+                offset,
+                "could not parse any expression variant",
+            )))
+    }
 }
 
-#[derive(Debug, Clone)]
-pub struct Expression<'a, 'c: 'a> {
-    ctx: &'a Context<'a, 'c>,
-    variant: Variant<'a>,
+#[derive(Debug, Clone, PartialEq)]
+pub struct Expression<'a> {
+    ctx: &'a Context,
+    variant: Variant,
 }
-impl<'a, 'c> Expression<'a, 'c> {
+impl<'a> Expression<'a> {
     pub fn infer(&self) -> Result<Type, InferenceError> {
         let mut tctx = polytype::Context::default();
         let env = VecDeque::new();
@@ -166,7 +316,7 @@ impl<'a, 'c> Expression<'a, 'c> {
         self.variant.show(self.ctx, is_function)
     }
 }
-impl<'a, 'c> fmt::Display for Expression<'a, 'c> {
+impl<'a> fmt::Display for Expression<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         write!(f, "{}", self.show(false))
     }
@@ -177,38 +327,199 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_infer() {
+    fn test_parse_primitive() {
         let ctx = Context::new(
-            hashmap![
-                "singleton" => arrow![tp!(0), tp!(list(tp!(0)))],
-                ">=" => arrow![tp!(int), tp!(int), tp!(bool)],
-                "+" => arrow![tp!(int), tp!(int), tp!(int)],
-                "0" => tp!(int),
-                "1" => tp!(int),
+            vec![
+                (String::from("singleton"), arrow![tp!(0), tp!(list(tp!(0)))]),
+            ],
+            vec![],
+        );
+        let expr = ctx.parse("singleton").unwrap();
+        assert_eq!(expr.variant, Variant::Primitive(0));
+
+        assert!(ctx.parse("something_else").is_err());
+        assert!(ctx.parse("singleton singleton").is_err());
+    }
+
+    #[test]
+    fn test_parse_application() {
+        let ctx = Context::new(
+            vec![
+                (String::from("singleton"), arrow![tp!(0), tp!(list(tp!(0)))]),
+                (String::from("thing"), arrow![tp!(int), tp!(int)]),
+            ],
+            vec![],
+        );
+        let expr = ctx.parse("(singleton singleton)").unwrap();
+        assert_eq!(
+            expr.variant,
+            Variant::Application(
+                Box::new(Variant::Primitive(0)),
+                Box::new(Variant::Primitive(0)),
+            )
+        );
+
+        // not a valid type, but that's not a guarantee the parser makes.
+        let expr = ctx.parse("(singleton thing singleton (singleton thing))")
+            .unwrap();
+        assert_eq!(
+            expr.variant,
+            Variant::Application(
+                Box::new(Variant::Application(
+                    Box::new(Variant::Application(
+                        Box::new(Variant::Primitive(0)),
+                        Box::new(Variant::Primitive(1)),
+                    )),
+                    Box::new(Variant::Primitive(0)),
+                )),
+                Box::new(Variant::Application(
+                    Box::new(Variant::Primitive(0)),
+                    Box::new(Variant::Primitive(1)),
+                )),
+            )
+        );
+
+        assert!(ctx.parse("()").is_err());
+    }
+
+    #[test]
+    fn test_parse_index() {
+        let ctx = Context::new(
+            vec![
+                (String::from("singleton"), arrow![tp!(0), tp!(list(tp!(0)))]),
+            ],
+            vec![],
+        );
+        let expr = ctx.parse("(singleton $0)").unwrap();
+        assert_eq!(
+            expr.variant,
+            Variant::Application(Box::new(Variant::Primitive(0)), Box::new(Variant::Index(0)))
+        );
+
+        /// an index never makes sense outside of an application or lambda body.
+        assert!(ctx.parse("$0").is_err());
+    }
+
+    #[test]
+    fn test_parse_invented() {
+        let ctx = Context::new(
+            vec![
+                (String::from("+"), arrow![tp!(int), tp!(int), tp!(int)]),
+                (String::from("1"), tp!(int)),
             ],
             vec![
                 (
                     Variant::Application(
-                        Box::new(Variant::Primitive("+")),
-                        Box::new(Variant::Primitive("1")),
+                        Box::new(Variant::Primitive(0)),
+                        Box::new(Variant::Primitive(1)),
+                    ),
+                    arrow![tp!(int), tp!(int)],
+                ),
+            ],
+        );
+        let expr = ctx.parse("(#(+ 1) 1)").unwrap();
+        assert_eq!(
+            expr.variant,
+            Variant::Application(
+                Box::new(Variant::Invented(0)),
+                Box::new(Variant::Primitive(1)),
+            )
+        );
+        assert!(ctx.parse("(#(+ 1 1) 1)").is_err());
+    }
+
+    #[test]
+    fn test_parse_abstraction() {
+        let ctx = Context::new(
+            vec![
+                (String::from("+"), arrow![tp!(int), tp!(int), tp!(int)]),
+                (String::from("1"), tp!(int)),
+            ],
+            vec![
+                (
+                    Variant::Abstraction(Box::new(Variant::Application(
+                        Box::new(Variant::Application(
+                            Box::new(Variant::Primitive(0)),
+                            Box::new(Variant::Application(
+                                Box::new(Variant::Application(
+                                    Box::new(Variant::Primitive(0)),
+                                    Box::new(Variant::Primitive(1)),
+                                )),
+                                Box::new(Variant::Primitive(1)),
+                            )),
+                        )),
+                        Box::new(Variant::Index(0)),
+                    ))),
+                    arrow![tp!(int), tp!(int)],
+                ),
+            ],
+        );
+        let expr = ctx.parse("(lambda (+ $0))").unwrap();
+        assert_eq!(
+            expr.variant,
+            Variant::Abstraction(Box::new(Variant::Application(
+                Box::new(Variant::Primitive(0)),
+                Box::new(Variant::Index(0)),
+            )))
+        );
+        let expr = ctx.parse("(#(lambda (+ (+ 1 1) $0)) ((lambda (+ $0 1)) 1))")
+            .unwrap();
+        assert_eq!(
+            expr.variant,
+            Variant::Application(
+                Box::new(Variant::Invented(0)),
+                Box::new(Variant::Application(
+                    Box::new(Variant::Abstraction(Box::new(Variant::Application(
+                        Box::new(Variant::Application(
+                            Box::new(Variant::Primitive(0)),
+                            Box::new(Variant::Index(0)),
+                        )),
+                        Box::new(Variant::Primitive(1)),
+                    )))),
+                    Box::new(Variant::Primitive(1)),
+                )),
+            ),
+        );
+        let expr = ctx.parse("(lambda $0)").unwrap();
+        assert_eq!(
+            expr.variant,
+            Variant::Abstraction(Box::new(Variant::Index(0)))
+        );
+    }
+
+    #[test]
+    fn test_infer() {
+        let ctx = Context::new(
+            vec![
+                (String::from("singleton"), arrow![tp!(0), tp!(list(tp!(0)))]),
+                (String::from(">="), arrow![tp!(int), tp!(int), tp!(bool)]),
+                (String::from("+"), arrow![tp!(int), tp!(int), tp!(int)]),
+                (String::from("0"), tp!(int)),
+                (String::from("1"), tp!(int)),
+            ],
+            vec![
+                (
+                    Variant::Application(
+                        Box::new(Variant::Primitive(2)),
+                        Box::new(Variant::Primitive(4)),
                     ),
                     arrow![tp!(int), tp!(int)],
                 ),
             ],
         );
         let v = Variant::Application(
-            Box::new(Variant::Primitive("singleton")),
+            Box::new(Variant::Primitive(0)),
             Box::new(Variant::Application(
                 Box::new(Variant::Abstraction(Box::new(Variant::Application(
                     Box::new(Variant::Application(
-                        Box::new(Variant::Primitive(">=")),
+                        Box::new(Variant::Primitive(1)),
                         Box::new(Variant::Index(0)),
                     )),
-                    Box::new(Variant::Primitive("1")),
+                    Box::new(Variant::Primitive(4)),
                 )))),
                 Box::new(Variant::Application(
                     Box::new(Variant::Invented(0)),
-                    Box::new(Variant::Primitive("0")),
+                    Box::new(Variant::Primitive(3)),
                 )),
             )),
         );
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
 #[macro_use]
 extern crate polytype;
 
-#[cfg(test)]
-#[macro_use]
-extern crate maplit;
-
 pub mod expression;
