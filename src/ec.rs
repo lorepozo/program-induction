08997d1554d781464d468c02a1cc930a1f09e590
Lucas Morales <lucas@lucasem.com>
2018-02-17 17:06:11 -0500

    change ec signature, add docs

--- src/ec.rs
+++ src/ec.rs
@@ -19,19 +19,32 @@ impl<'a, T> Task<'a, T> {
     }
 }
 
+/// Lots of type variables here.
+///
+/// - `S` is some "state" initially passed in as a prior, probably taking the form of production
+///   probabilities. But it's ultimately up to you what it is and how it gets used. The important
+///   thing is that it's something you can change during ec at any point in the pipeline (which
+///   will probably be just compression updating production probabilities and adding to the DSL).
+/// - `O` is the observation type, something which the recognizer/explorer can take advantage of
+///   instead of just basing search off of the type and oracle response.
+/// - `RS` is something the recognizer returns that the enumerator can use. This could be, for
+///   example, a map from task number to a set of production probabilities.
+/// - `R`, `E`, and `C` are the types, described in the `where` clause of the function signature
+///   here, for a recognizer, an enumerator, and a compressor.
 pub fn ec<S, O, RS, R, E, C>(
-    prior: &(DSL, S),
+    prior: &DSL,
+    mut state: &mut S,
     tasks: &Vec<Task<O>>,
     recognizer: Option<R>,
     explore: E,
     compress: C,
-) -> (DSL, S)
+) -> DSL
 where
-    R: FnOnce(&(DSL, S), &Vec<Task<O>>) -> RS,
-    E: FnOnce(&(DSL, S), &Vec<Task<O>>, Option<RS>) -> HashMap<usize, Vec<Expression>>,
-    C: FnOnce(&(DSL, S), &Vec<Task<O>>, HashMap<usize, Vec<Expression>>) -> (DSL, S),
+    R: FnOnce(&DSL, &mut S, &Vec<Task<O>>) -> RS,
+    E: FnOnce(&DSL, &mut S, &Vec<Task<O>>, Option<RS>) -> HashMap<usize, Vec<Expression>>,
+    C: FnOnce(&DSL, &mut S, &Vec<Task<O>>, HashMap<usize, Vec<Expression>>) -> DSL,
 {
-    let recognized = recognizer.map(|r| r(prior, tasks));
-    let frontiers = explore(prior, tasks, recognized);
-    compress(prior, tasks, frontiers)
+    let recognized = recognizer.map(|r| r(prior, &mut state, tasks));
+    let frontiers = explore(prior, &mut state, tasks, recognized);
+    compress(prior, &mut state, tasks, frontiers)
 }
