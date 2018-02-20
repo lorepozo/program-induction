38c4c590ecdc28f4ef78b225a3275cd7aec5ae85
Lucas Morales <lucas@lucasem.com>
2018-02-20 17:50:38 -0500

    enumeration

--- src/ec.rs
+++ src/ec.rs
@@ -1,8 +1,13 @@
 use std::collections::{HashMap, VecDeque};
 use std::f64;
+use std::iter;
+use std::rc::Rc;
 use polytype::{Context, Type};
 
 use super::{Expression, Task, DSL};
+
+const BUDGET_INCREMENT: f64 = 1.0;
+const MAX_DEPTH: u32 = 256;
 
 /// The entry point for one iteration of the EC algorithm.
 ///
@@ -171,6 +176,9 @@ pub struct State {
     pub search_limit: usize,
 }
 
+/// Enumerate solutions for the given tasks. Considers a "solution" to be any expression with
+/// finite log-probability according to a task's oracle. Each task will be associated with at most
+/// `frontier_size` many such expressions.
 pub fn explore<O>(
     dsl: &DSL,
     state: &mut State,
@@ -183,10 +191,10 @@ pub fn explore<O>(
             .zip(productions)
             .enumerate()
             .map(|(i, (ref t, ref p))| {
-                enumerate_tp(
+                enumerate_solutions(
                     dsl,
                     p,
-                    &t.tp,
+                    t.tp.clone(),
                     vec![(i, t)],
                     state.frontier_size,
                     state.search_limit,
@@ -202,10 +210,10 @@ pub fn explore<O>(
         let mut results = vec![vec![]; tasks.len()];
         for (i, exprs) in tps.into_iter()
             .map(|(tp, tasks)| {
-                enumerate_tp(
+                enumerate_solutions(
                     dsl,
                     &state.productions,
-                    &tp,
+                    tp.clone(),
                     tasks,
                     state.frontier_size,
                     state.search_limit,
@@ -219,13 +227,189 @@ pub fn explore<O>(
     }
 }
 
-fn enumerate_tp<O>(
-    _dsl: &DSL,
-    _productions: &Productions,
-    _tp: &Type,
-    _tasks: Vec<(usize, &Task<O>)>,
-    _frontier_size: usize,
-    _search_limit: usize,
+fn enumerate_solutions<O>(
+    dsl: &DSL,
+    productions: &Productions,
+    tp: Type,
+    mut tasks: Vec<(usize, &Task<O>)>,
+    frontier_size: usize,
+    search_limit: usize,
 ) -> HashMap<usize, Vec<Expression>> {
-    HashMap::new()
+    let mut frontier = HashMap::new();
+    let mut searched = 0;
+    let mut update = |frontier: &mut HashMap<_, _>, expr: Expression| {
+        tasks.retain(|&(i, t)| {
+            if (t.oracle)(&expr, dsl).is_finite() {
+                let v = frontier.entry(i).or_insert(Vec::new());
+                v.push(expr.clone());
+                v.len() < frontier_size
+            } else {
+                true
+            }
+        });
+        if tasks.is_empty() {
+            false
+        } else {
+            searched += 1;
+            searched < search_limit
+        }
+    };
+    let ref enumerator = Enumerator::new(tp, dsl, productions);
+    for expr in enumerator {
+        if !update(&mut frontier, expr) {
+            break;
+        }
+    }
+    frontier
+}
+
+#[derive(Debug, Clone)]
+struct LinkedList<T: Clone>(Option<(T, Rc<LinkedList<T>>)>);
+impl<T: Clone> LinkedList<T> {
+    fn prepend(lst: Rc<LinkedList<T>>, v: T) -> Rc<LinkedList<T>> {
+        Rc::new(LinkedList(Some((v, lst.clone()))))
+    }
+    fn as_vecdeque(mut lst: &Rc<LinkedList<T>>) -> VecDeque<T> {
+        let mut out = VecDeque::new();
+        loop {
+            if let Some((ref v, ref nlst)) = lst.0 {
+                out.push_back(v.clone());
+                lst = nlst;
+            } else {
+                break;
+            }
+        }
+        out
+    }
+}
+impl<T: Clone> Default for LinkedList<T> {
+    fn default() -> Self {
+        LinkedList(None)
+    }
+}
+
+struct Enumerator<'a> {
+    tp: Type,
+    dsl: &'a DSL,
+    productions: &'a Productions,
+    /// budget (lower, upper) in nats, i.e. negative log-likelihood.
+    budget: (f64, f64),
+}
+impl<'a> Enumerator<'a> {
+    fn new(tp: Type, dsl: &'a DSL, productions: &'a Productions) -> Self {
+        Self {
+            tp,
+            dsl,
+            productions,
+            budget: (0f64, 0f64 + BUDGET_INCREMENT),
+        }
+    }
+    fn at_depth(&'a self, depth: f64) -> Box<Iterator<Item = Expression> + 'a> {
+        let (l, h) = self.budget;
+        let budget = (l + depth, h + depth);
+        let ctx = Context::default();
+        let env = Rc::new(LinkedList::default());
+        Box::new(
+            self.enumerator(self.tp.clone(), &ctx, env, budget, 0)
+                .map(|(_, _, expr)| expr),
+        )
+    }
+    fn enumerator(
+        &'a self,
+        request: Type,
+        ctx: &Context,
+        env: Rc<LinkedList<Type>>,
+        budget: (f64, f64),
+        depth: u32,
+    ) -> Box<Iterator<Item = (f64, Context, Expression)> + 'a> {
+        if let Type::Arrow(arrow) = request {
+            let env = LinkedList::prepend(env, *arrow.arg);
+            let it = self.enumerator(*arrow.ret, ctx, env, budget, depth)
+                .map(|(ll, ctx, body)| (ll, ctx, Expression::Abstraction(Box::new(body))));
+            Box::new(it)
+        } else {
+            let it = self.productions
+                .candidates(
+                    self.dsl,
+                    &request,
+                    ctx,
+                    &LinkedList::as_vecdeque(&env),
+                    false,
+                )
+                .into_iter()
+                .filter(move |&(ll, _, _, _)| -ll > budget.1)
+                .flat_map(
+                    move |(ll, expr, tp, ctx)| -> Box<Iterator<Item = (f64, Context, Expression)> + 'a> {
+                        let budget = (budget.0 + ll, budget.1 + ll);
+                        if budget.1 <= 0f64 || depth > MAX_DEPTH {
+                            Box::new(iter::empty())
+                        } else if let Type::Arrow(f_tp) = tp {
+                            let arg_tps: VecDeque<Type> = f_tp.args().into_iter().cloned().collect();
+                            self.enumerate_application(
+                                &ctx,
+                                env.clone(),
+                                expr,
+                                ll,
+                                arg_tps,
+                                budget,
+                                depth + 1,
+                            )
+                        } else if budget.0 < 0f64 && 0f64 <= budget.1 {
+                            Box::new(iter::once((ll, ctx, expr)))
+                        } else {
+                            Box::new(iter::empty())
+                        }
+                    },
+                );
+            Box::new(it)
+        }
+    }
+    fn enumerate_application(
+        &'a self,
+        ctx: &Context,
+        env: Rc<LinkedList<Type>>,
+        f: Expression,
+        f_ll: f64,
+        mut arg_tps: VecDeque<Type>,
+        budget: (f64, f64),
+        depth: u32,
+    ) -> Box<Iterator<Item = (f64, Context, Expression)> + 'a> {
+        if arg_tps.is_empty() {
+            if budget.0 < 0f64 && 0f64 <= budget.1 {
+                Box::new(iter::once((f_ll, ctx.clone(), f)))
+            } else {
+                Box::new(iter::empty())
+            }
+        } else {
+            let arg_tp = arg_tps.pop_front().unwrap();
+            let budget = (0f64, budget.1);
+            let it = self.enumerator(arg_tp, ctx, env.clone(), budget, depth)
+                .flat_map(move |(arg_ll, ctx, arg)| {
+                    let f_next = Expression::Application(Box::new(f.clone()), Box::new(arg));
+                    let budget = (budget.0 + arg_ll, budget.1 + arg_ll);
+                    self.enumerate_application(
+                        &ctx,
+                        env.clone(),
+                        f_next,
+                        f_ll + arg_ll,
+                        arg_tps.clone(),
+                        budget,
+                        depth,
+                    )
+                });
+            Box::new(it)
+        }
+    }
+}
+impl<'a> IntoIterator for &'a Enumerator<'a> {
+    type Item = Expression;
+    type IntoIter = Box<Iterator<Item = Expression> + 'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Box::new(
+            (0..)
+                .map(|n| BUDGET_INCREMENT * (n as f64))
+                .flat_map(move |depth| self.at_depth(depth)),
+        )
+    }
 }
